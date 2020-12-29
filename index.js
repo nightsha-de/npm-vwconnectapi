e66a8a2809c67262c4b2b0bfb40a48ddb43f076f
nightsha-de <64610344+nightsha-de@users.noreply.github.com>
2020-12-29 21:32:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const { JSDOM } = jsdom;
 
 class VwWeConnect {
     constructor() {
-        this.on("ready", this.onReady.bind(this));
-        // this.on("objectChange", this.onObjectChange.bind(this));
-        this.on("stateChange", this.onStateChange.bind(this));
-        // this.on("message", this.onMessage.bind(this));
-        this.on("unload", this.onUnload.bind(this));
+        //this.on("ready", this.onReady.bind(this));
+        //// this.on("objectChange", this.onObjectChange.bind(this));
+        //this.on("stateChange", this.onStateChange.bind(this));
+        //// this.on("message", this.onMessage.bind(this));
+        //this.on("unload", this.onUnload.bind(this));
         
         this.jar = request.jar();
 
